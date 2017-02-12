c1ecffda7c03e3c8dbb2792a21aa201bd96842a8
lizziedaisy <Lizzie@Lizzies-MacBook-Air.local>
2017-02-12 11:10:40 +0100

    Evening Version No Blog Content

--- js/script.js
+++ js/script.js
@@ -29,13 +29,10 @@
     nav:false,
     items:1,
     autoplay:true,
-  })
-});
+  });
 
-$( document ).ready(function() {
-   $(".dropdown-nav").click(function(){
-     $(this).children(".dropdown-content").slideToggle();
-   });
+  
+
 });
 
 
